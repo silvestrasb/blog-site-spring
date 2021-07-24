5f8d5ecbdc4ddd59994613afef5a86db94ca2514
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-07-24 22:01:12 +0300

    BlogController - added methods for creating and viewing blogs.

--- src/main/java/lt/codeacademy/blog_site/controller/BlogController.java
+++ src/main/java/lt/codeacademy/blog_site/controller/BlogController.java
@@ -1,30 +1,51 @@
 package lt.codeacademy.blog_site.controller;
 
 import lt.codeacademy.blog_site.entity.Blog;
+import lt.codeacademy.blog_site.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 @RequestMapping("/blogs")
 public class BlogController {
 
+    private final BlogService blogService;
+
+    public BlogController(BlogService blogService) {
+        this.blogService = blogService;
+    }
+
     @GetMapping
-    public String getBlogs(){
+    public String getBlogs(Model model) {
+
+
+        List<Blog> sortedList = blogService
+                .getAll()
+                .stream()
+                .sorted(Comparator.comparingLong(Blog::getId).reversed())
+                .collect(Collectors.toList());
+
+        model.addAttribute("blogList", sortedList);
         return "blog/index";
     }
 
     @GetMapping("/create")
-    public String createBlog(Model model){
+    public String createBlog(Model model) {
         model.addAttribute("blog", new Blog());
         return "blog/create";
     }
 
     @PostMapping("/create")
-    public String createBlog(){
-        return "blog/view";
+    public String createBlog(Blog blog) {
+        blogService.saveBlog(blog);
+        return "redirect:/blogs";
     }
 
 }
