1985c0a5283fd795a7dbb899a0b5c5a3fc783247
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-07-24 17:59:55 +0300

    File structure set-up.

---
+++ src/main/java/lt/codeacademy/blog_site/BlogSiteHwApplication.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.blog_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSiteHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSiteHwApplication.class, args);
+    }
+
+}
