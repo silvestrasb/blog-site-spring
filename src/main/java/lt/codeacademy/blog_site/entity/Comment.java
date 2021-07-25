72d8c56de4a94ca233c5b7b0e6e377f1ba6b1811
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-07-25 21:25:15 +0300

    Added Coment entity/repository/service.

---
+++ src/main/java/lt/codeacademy/blog_site/entity/Comment.java
@@ -0,0 +1,22 @@
+package lt.codeacademy.blog_site.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "blog_id")
+    private Long blogId;
+
+}
---
+++ src/main/java/lt/codeacademy/blog_site/service/CommentService.java
@@ -0,0 +1,27 @@
+package lt.codeacademy.blog_site.service;
+
+import lt.codeacademy.blog_site.entity.Blog;
+import lt.codeacademy.blog_site.entity.Comment;
+import lt.codeacademy.blog_site.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+//
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+//
+    public List<Comment> getAllByBlogId(Long blogId) {
+        return commentRepository.getAllByBlogId(blogId);
+    }
+
+    public Comment save(Comment comment){
+        return commentRepository.save(comment);
+    }
+
+}
