98f2dba91cb3d842c8d17b819226442a4bb46a6a
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-07-29 22:24:34 +0300

    Updated BlogController.

--- src/main/java/lt/codeacademy/blog_site/controller/BlogController.java
+++ src/main/java/lt/codeacademy/blog_site/controller/BlogController.java
@@ -11,10 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Controller
 @RequestMapping("/blogs")
 public class BlogController {
@@ -30,19 +26,7 @@ public class BlogController {
 
     @GetMapping
     public String getBlogs(Model model) {
-
-        List<Blog> sortedList = blogService
-                .getAll()
-                .stream()
-                .map(blog -> {
-                            blog.setContent(blog.getContent().substring(0, Math.min(blog.getContent().length(), 500)));
-                            return blog;
-                        }
-                )
-                .sorted(Comparator.comparingLong(Blog::getId).reversed())
-                .collect(Collectors.toList());
-
-        model.addAttribute("blogList", sortedList);
+        model.addAttribute("blogList", blogService.getAllFirstNCharacters(100));
         return "blog/index";
     }
 
@@ -53,9 +37,8 @@ public class BlogController {
     }
 
     @GetMapping("/{id}/view")
-    public String getBlog(@PathVariable("id") Blog blog, Model model){
+    public String getBlog(@PathVariable("id") Blog blog, Model model) {
         model.addAttribute("blog", blog);
-        model.addAttribute("comments", commentService.getAllByBlogId(blog.getId()));
         model.addAttribute("commentBox", new Comment());
         return "blog/view";
     }
@@ -63,12 +46,12 @@ public class BlogController {
     @GetMapping("/{id}/edit")
     public String editBlog(@PathVariable("id") Blog blog, Model model){
         model.addAttribute("blog", blog);
-        return "blog/create";
+        return "blog/edit";
     }
 
 
     @GetMapping("/{id}/delete")
-    public String deleteCar(@PathVariable("id") Blog blog) {
+    public String deleteBlog(@PathVariable("id") Blog blog) {
         blogService.deleteBlog(blog);
         return "redirect:/blogs";
     }
@@ -79,12 +62,12 @@ public class BlogController {
         return "redirect:/blogs";
     }
 
-    @PostMapping("/{id}/add-comment")
-    public String addComment(@PathVariable("id") Blog blog, Comment comment) {
-        comment.setId(null);
-        comment.setBlogId(blog.getId());
-        commentService.save(comment);
+    @PostMapping("/{id}/edit")
+    public String editBlog(@PathVariable("id") Blog dbBlog, Blog blog) {
+        blog.setComments(dbBlog.getComments());
+        blogService.saveBlog(blog);
         return "redirect:/blogs/" + blog.getId() + "/view";
-
     }
+
+
 }
