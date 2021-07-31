5f6167895def3fc9b517325362e1b3f1b1fac49a
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-08-01 02:24:47 +0300

    New custom exception UserNotFoundException.

---
+++ src/main/java/lt/codeacademy/blog_site/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package lt.codeacademy.blog_site.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    private final String username;
+    public UserNotFoundException(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
