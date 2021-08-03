6bcaf4168ae716529e71e9102cc8aeacc67a1a85
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-08-03 17:48:56 +0300

    Refactoring of code.

--- src/main/java/lt/codeacademy/blog_site/repository/CommentRepository.java
+++ src/main/java/lt/codeacademy/blog_site/repository/CommentRepository.java
@@ -1,13 +1,9 @@
 package lt.codeacademy.blog_site.repository;
 
-import lt.codeacademy.blog_site.entity.Blog;
 import lt.codeacademy.blog_site.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> getAllByBlogId(Long blogId);
 }
--- src/main/java/lt/codeacademy/blog_site/service/CommentService.java
+++ src/main/java/lt/codeacademy/blog_site/service/CommentService.java
@@ -4,8 +4,6 @@ import lt.codeacademy.blog_site.entity.Comment;
 import lt.codeacademy.blog_site.repository.CommentRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class CommentService {
 
@@ -15,18 +13,10 @@ public class CommentService {
         this.commentRepository = commentRepository;
     }
 
-    public List<Comment> getAllByBlogId(Long blogId) {
-        return commentRepository.getAllByBlogId(blogId);
-    }
-
     public Comment save(Comment comment){
         return commentRepository.save(comment);
     }
 
-//    TEMP:
-    public Comment getCommentById(Long id){
-        return commentRepository.getById(id);
-    }
     public void deleteById(Long blogId){
         commentRepository.deleteById(blogId);
     }
